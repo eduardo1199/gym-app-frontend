7116b80b88e210d8a383a05adcc8e3e2c59f03a6
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2022-10-08 15:34:24 -0300

    Remover utils de cpf mask

--- src/utils/mark.ts
+++
@@ -1,8 +0,0 @@
-export function cpfMask(cpf: string) {
-  return cpf
-  .replace(/\D/g, '') 
-  .replace(/(\d{3})(\d)/, '$1.$2')
-  .replace(/(\d{3})(\d)/, '$1.$2')
-  .replace(/(\d{3})(\d{1,2})/, '$1-$2')
-  .replace(/(-\d{2})\d+?$/, '$1')
-}
