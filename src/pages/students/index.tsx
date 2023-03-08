067732e70951daff60c443b2f212af5014b5d529
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-08 08:47:18 -0300

    styled input and label modal

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react'
 import { AlertConfirm } from '../../components/AlertConfirm'
 import { Header } from '../../components/Header'
 import { Modal } from '../../components/Modal'
-import { TableRow } from '../../components/Table/TableRow'
+import { TableRow } from './components/Table/TableRow'
 import {
   useGetUsersQuery,
   useDeleteUserMutation,
@@ -12,8 +12,6 @@ import { dateFormat } from '../../utils'
 
 export function Students() {
   const [userId, setUserId] = useState('')
-
-  const toast = useToast()
 
   const { data: users } = useGetUsersQuery()
   const [handleDeleteUserMutation, { isLoading, isSuccess, isError }] =
@@ -25,6 +23,8 @@ export function Students() {
     onOpen: onOpenModalEdit,
     onClose: onCloseModalEdit,
   } = useDisclosure()
+
+  const toast = useToast()
 
   function handleOpenAlertConfirm(id: string) {
     setUserId(id)
@@ -122,25 +122,87 @@ export function Students() {
         onCloseModalEdit={onCloseModalEdit}
         ref={ref}
         handleSubmit={() => {}}
+        visibleButtonsFooter={false}
       >
         <form action="" className="flex flex-col gap-2" onSubmit={() => {}}>
-          <label htmlFor="name">Nome do alunos</label>
-          <input type="text" id="name" placeholder="Nome do aluno" />
+          <label htmlFor="name" className="text-white font-semibold">
+            Nome
+          </label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Nome do aluno"
+            className="p-2 rounded placeholder:text-white placeholder:text-sm placeholder:text-opacity-60 bg-primary-purple brightness-125 text-white text-sm focus:outline-none focus:ring focus:ring-purple-700"
+          />
 
-          <label htmlFor="age">Idade</label>
-          <input type="number" id="age" placeholder="Idade do aluno" />
+          <label htmlFor="age" className="text-white font-semibold">
+            Idade
+          </label>
+          <input
+            type="number"
+            id="age"
+            placeholder="Idade do aluno"
+            className="p-2 rounded placeholder:text-white placeholder:text-sm placeholder:text-opacity-60 bg-primary-purple brightness-125 text-white text-sm focus:outline-none focus:ring focus:ring-purple-700"
+          />
 
-          <label htmlFor="weight">Peso</label>
-          <input type="text" id="weight" placeholder="Peso do aluno" />
+          <label htmlFor="weight" className="text-white font-semibold">
+            Peso
+          </label>
+          <input
+            type="text"
+            id="weight"
+            placeholder="Peso do aluno"
+            className="p-2 rounded placeholder:text-white placeholder:text-sm placeholder:text-opacity-60 bg-primary-purple brightness-125 text-white text-sm focus:outline-none focus:ring focus:ring-purple-700"
+          />
 
-          <label htmlFor="cpf">CPF</label>
-          <input type="text" id="cpf" placeholder="CPF do aluno" />
+          <label htmlFor="cpf" className="text-white font-semibold">
+            CPF
+          </label>
+          <input
+            type="text"
+            id="cpf"
+            placeholder="CPF do aluno"
+            className="p-2 rounded placeholder:text-white placeholder:text-sm placeholder:text-opacity-60 bg-primary-purple brightness-125 text-white text-sm focus:outline-none focus:ring focus:ring-purple-700"
+          />
 
-          <label htmlFor="plan">Plano</label>
-          <input type="text" id="plan" placeholder="Plano do aluno" />
+          <label htmlFor="plan" className="text-white font-semibold">
+            Plano
+          </label>
+          <input
+            type="text"
+            id="plan"
+            placeholder="Plano do aluno"
+            className="p-2 rounded placeholder:text-white placeholder:text-sm placeholder:text-opacity-60 bg-primary-purple brightness-125 text-white text-sm focus:outline-none focus:ring focus:ring-purple-700"
+          />
 
-          <label htmlFor="startDateForPlan">Data de início do plano</label>
-          <input type="date" id="startDateForPlan" />
+          <label
+            htmlFor="startDateForPlan"
+            className="text-white font-semibold"
+          >
+            Data de início do plano
+          </label>
+          <input
+            type="date"
+            id="startDateForPlan"
+            className="p-2 rounded placeholder:text-white placeholder:text-sm placeholder:text-opacity-60 bg-primary-purple brightness-125 text-white text-sm focus:outline-none focus:ring focus:ring-purple-700"
+          />
+
+          <div className="flex justify-between my-4">
+            <button
+              type="button"
+              onClick={onCloseModalEdit}
+              className="bg-secondary-orange p-2 rounded text-base font-bold text-white hover:bg-orange-400 transition-colors focus:outline-none focus:ring focus:ring-orange-300"
+            >
+              Cancelar
+            </button>
+
+            <button
+              type="submit"
+              className="bg-primary-yellow p-2 rounded text-base font-bold text-white hover:bg-secondary-yellow transition-colors focus:outline-none focus:ring focus:ring-yellow-300"
+            >
+              Cadastrar
+            </button>
+          </div>
         </form>
       </Modal>
     </>
