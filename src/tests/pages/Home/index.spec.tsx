f6841a9091511fcfeb6013bc7b33fe79a70be8a7
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-12 21:26:19 -0300

    tests on page home of the component form

--- src/tests/pages/Home/index.spec.tsx
+++ src/tests/pages/Home/index.spec.tsx
@@ -1,7 +1,29 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Home } from '../../../pages/Home'
 
-it('should be text in the home', () => {
-  const { getByTestId } = render(<Home />)
-  expect(getByTestId('home-form-title')).toBeInTheDocument()
+describe('Test Component Home Page Form', () => {
+  it('should be render title form and select profile buttons and Inputs', () => {
+    const { getByTestId, getByTitle, getByLabelText, queryByLabelText } =
+      render(<Home />)
+
+    expect(getByTestId('home-form-title')).toBeInTheDocument()
+    expect(getByTitle('aluno')).toBeInTheDocument()
+    expect(getByTitle('gerente')).toBeInTheDocument()
+    expect(getByLabelText('Informe seu CPF')).toBeInTheDocument()
+    expect(queryByLabelText('Informe sua senha')).toBeNull()
+  })
+
+  it('should be render input password after select profile admin', async () => {
+    const { getByTitle, getByLabelText, queryByLabelText } = render(<Home />)
+
+    expect(getByTitle('gerente')).toBeInTheDocument()
+
+    const buttonSelectProfileAdmin = getByTitle('gerente')
+
+    await userEvent.click(buttonSelectProfileAdmin)
+
+    expect(getByLabelText('Informe seu CPF')).toBeInTheDocument()
+    expect(queryByLabelText('Informe sua senha')).toBeInTheDocument()
+  })
 })
