a40477bf076b75d1b17e9379bc82ed7474b16b96
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-04-09 22:12:35 -0300

    feat: resolve request plans

--- src/feature/plan/plan-slice.ts
+++ src/feature/plan/plan-slice.ts
@@ -4,13 +4,17 @@ import { axiosBaseQuery } from '../../services/axiosBaseQuery'
 
 import { Plan } from '../../types/plan'
 
+interface GetPlansResponse {
+  plans: Plan[]
+}
+
 export const apiPlanSlice = createApi({
   reducerPath: 'api-plans',
   baseQuery: axiosBaseQuery({
     baseUrl: import.meta.env.VITE_API_URL,
   }),
   endpoints: (builder) => ({
-    GetPlans: builder.query<Plan[], void>({
+    GetPlans: builder.query<GetPlansResponse, void>({
       query: () => ({
         url: 'plans',
         method: 'GET',
