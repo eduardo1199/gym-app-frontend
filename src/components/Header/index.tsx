7b2ea8d1f72eaf71b5bbdad6a8a32c075abf95a0
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-04-30 20:40:31 -0300

    feat: adjusted components alert, header and input search

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,21 +5,15 @@ import { titleFromPage } from './utils'
 import { useSelector } from 'react-redux'
 import { RootState } from 'src/app/store'
 
-interface HeaderProps {
-  visibleSearchBar: boolean
-}
-
-export function Header({ visibleSearchBar = false }: HeaderProps) {
+export function Header() {
   const { pathname } = useLocation()
 
   const admin = useSelector((state: RootState) => state.admin)
 
-  console.log(visibleSearchBar)
-
   const title = titleFromPage(pathname)
 
   return (
-    <header className="flex h-[50px] justify-between mb-4 p-8">
+    <header className="flex items-center h-[50px] justify-between mb-4 p-8">
       <h1 className="font-bold text-3xl text-secondary-blue">{title}</h1>
 
       <Logout userName={admin?.name ?? ''} />
